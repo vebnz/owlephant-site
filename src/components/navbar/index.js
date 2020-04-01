9f818022beb43c089c1ef2566ed758ae6f1c9eff
Ray Arvin Rimorin <rayarvin@gmail.com>
2020-04-01 08:58:55 +0800

    Update _variable.scss, styles.css, and 3 more files...

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,7 +5,8 @@ import {
   Form,
   FormControl,
   Button,
-  NavDropdown
+  NavDropdown,
+  Container
 } from "react-bootstrap";
 
 const OwlNavbar = (props) => {
@@ -16,25 +17,27 @@ console.log("OwlNavbar -> props", props)
       expand="lg"
       fixed="top"
       variant={props.dark ? "dark" : "light"}
-      className={`theme-nav`}
+      className={`theme-nav blurry`}
     >
-      <Navbar.Brand href="#home">
-        <img
-          alt=""
-          src={`/assets/images/owl${!props.dark ? "dark" : "white"}.svg`}
-          width="50"
-          height="50"
-          className="d-inline-block align-top"
-        />{" "}
-        <span className="navbar-brand-name">owlephant</span>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+      <Container fluid className="">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={`/assets/images/owl${!props.dark ? "dark" : "white"}.svg`}
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+          />{" "}
+          <span className="navbar-brand-name">owlephant</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
