69942e81116a306e6757728894a1308706dba9b0
Mike Mackenzie <mike@diastorm.com>
2020-04-01 20:13:12 +1300

    Updating information about team members
    Updating information about team members

--- pages/about.js
+++ pages/about.js
@@ -37,21 +37,34 @@ const About = props => {
                     software and social media combine here in New Zealand.
                   </p>
                   <p>
-                    A practical person, with a knack of solving real world
-                    problems with Information Technology. He thinks the time is
-                    perfect for people to make the next jump into managing their
-                    health better
+                    A practical person, with a knack of solving real world problems
+                    with Information Technology. He thinks the time is perfect
+                    for people to make the next jump into managing their health
+                    better. He's obsessed with accessibility and wants to
+                    make the world more accessible to those with disabilities -
+                    from personal experience, it makes his ambition even stronger.
+                  </p>
+                  <p>
+                    Mike also created a group called CaptionItNZ, in which he made notable
+                    companies such as Air New Zealand,
+                    Slingshot and Spark create captions for their social media videos via
+                    educating them on why it's a good idea. Not just for those who are deaf,
+                    but for those who are foreign as captions can be easily translated.
+                    They are still doing it to this day.
                   </p>
                 </Card.Text>
               </Card.Body>
               <ListGroup variant="flush">
-                <ListGroup.Item>Has a BInfoTech degree</ListGroup.Item>
+                <ListGroup.Item>Has a BInfoTech degree from Otago Polytechnic</ListGroup.Item>
                 <ListGroup.Item>
                   Built up some sizable online communities for games, tv shows,
                   potato chips, you name it
                 </ListGroup.Item>
                 <ListGroup.Item>
                   Has spent more than 12 years as a software developer
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Has spent the last 3 years creating React-Native applications.
                 </ListGroup.Item>
                 <ListGroup.Item>Seemingly knows what he's doing</ListGroup.Item>
               </ListGroup>
@@ -96,6 +109,9 @@ const About = props => {
                   mobile startup in the Philippines
                 </ListGroup.Item>
                 <ListGroup.Item>
+                  Has spent the last 3 years creating React applications with and Django.
+                </ListGroup.Item>
+                <ListGroup.Item>
                   Has very little clue of what he's doing
                 </ListGroup.Item>
               </ListGroup>
@@ -114,19 +130,26 @@ const About = props => {
               </Card.Body>
               <Card.Body>
                 <Card.Text>
-                  <p>
-                    He olden day before flaunting finds mote, he made sorrow
-                    neer yet, cared smile pillared like given long he, a.
+                  <p>x`
+                  After working for the New Zealand Stock Exchange as a DevOps
+                  analyst helping manage, operate, and maintain all systems in t
+                  he exchange. Jamie also worked as Senior Web Developer for the
+                  Agri division of NZX leading the development for their three websites.
+                    <p>
+                      Jamie moved to Bahrain in 2015 and continued his Web Development role remotely.
+                      In 2017 Jamie left to be part of a FinTech startup in Bahrain.
+                    </p>
                   </p>
                   <p>
-                    His ere one where there and birth drowsy. Massy lands the
-                    grace these, been and sadness done his. Nor like vile flow
-                    such dote rill carnal gild. Reverie break men.
+                    Jamie has a very strong background in DevOps and Development and has spent the last 3 years managing a
+                    development team in the digitizing of a FinTech company and developing a self service portal for its clients.
+                    Jamie is a very proficient and strong manager with heavy skills in molding and mentoring teams, communicating with
+                    clients, and working with all aspects of the business and project.
                   </p>
                 </Card.Text>
               </Card.Body>
               <ListGroup variant="flush">
-                <ListGroup.Item>Has a BInfoTech degree</ListGroup.Item>
+                <ListGroup.Item>Has a BICT degree from UCol</ListGroup.Item>
                 <ListGroup.Item>
                   Built up some sizable online communities for games, tv shows,
                   potato chips, you name it
@@ -157,10 +180,10 @@ const About = props => {
                 </div>
                 <p>Powered by the following technologies</p>
                 <ListGroup variant="flush" horizontal>
-                  <ListGroup.Item>React.js</ListGroup.Item>
-                  <ListGroup.Item>Next.js</ListGroup.Item>
-                  <ListGroup.Item>Bootstrap 4</ListGroup.Item>
-                  <ListGroup.Item>Caffeine</ListGroup.Item>
+                  <ListGroup.Item action href="https://reactjs.org/">React.js</ListGroup.Item>
+                  <ListGroup.Item action href="https://nextjs.org/">Next.js</ListGroup.Item>
+                  <ListGroup.Item action href="https://getbootstrap.com/">Bootstrap 4</ListGroup.Item>
+                  <ListGroup.Item>Caffeinated beverages</ListGroup.Item>
                 </ListGroup>
               </Col>
             </Row>
