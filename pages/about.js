040d1ad25dc01208fad65dec4d62c38de599092a
Ray Arvin Rimorin <rayarvin@gmail.com>
2020-04-01 14:56:55 +0800

    Update about.js copty

--- pages/about.js
+++ pages/about.js
@@ -32,19 +32,30 @@ const About = props => {
                   <p>
                     Mike has spent time developing software and networks within
                     the New Zealand healthcare industry. As someone who has been
-                    deaf since birth, and recently received his first Cochlear
-                    Implant, Mike's got a very good grasp on how the market,
-                    software and social media combine here in New Zealand.
+                    deaf since birth, and received a Cochlear Implant, Mike's
+                    got a very good grasp on how business, software and social
+                    media combine here in New Zealand and abroad.
                   </p>
                   <p>
                     A practical person, with a knack of solving real world
                     problems with Information Technology. He thinks the time is
                     perfect for people to make the next jump into managing their
-                    health better
+                    health better. He's obsessed with accessibility and wants to
+                    make the world more accessible to those with disabilities -
+                    from personal experience, it makes his ambition even
+                    stronger.
+                  </p>
+                  <p>
+                    Mike created a group called CaptionItNZ, in which he made
+                    notable companies such as Air New Zealand, Slingshot and
+                    Spark create captions for their social media videos via
+                    educating them on why it's a good idea. Not just for those
+                    who are deaf, but for those who are foreign as captions can
+                    be easily translated. They are still doing it to this day.
                   </p>
                 </Card.Text>
               </Card.Body>
-              <ListGroup variant="flush">
+              <ListGroup>
                 <ListGroup.Item>Has a BInfoTech degree</ListGroup.Item>
                 <ListGroup.Item>
                   Built up some sizable online communities for games, tv shows,
@@ -78,13 +89,14 @@ const About = props => {
                     writer for humor magazines
                   </p>
                   <p>
-                    An inquisitive person with a wide range of interests. He
-                    thinks its time to stir up the health sector with the
-                    release of new consumer tech especially wearables
+                    An inquisitive person with a wide range of interests. Front
+                    end, backend, devops, graphic design; a passable full stack
+                    developer if there ever was one
                   </p>
+                  <p>A part time designer/digital artist</p>
                 </Card.Text>
               </Card.Body>
-              <ListGroup variant="flush">
+              <ListGroup>
                 <ListGroup.Item>
                   Started with Java apps for feature phones. Yes, he's a hipster
                 </ListGroup.Item>
@@ -125,7 +137,7 @@ const About = props => {
                   </p>
                 </Card.Text>
               </Card.Body>
-              <ListGroup variant="flush">
+              <ListGroup>
                 <ListGroup.Item>Has a BInfoTech degree</ListGroup.Item>
                 <ListGroup.Item>
                   Built up some sizable online communities for games, tv shows,
@@ -156,7 +168,7 @@ const About = props => {
                   <h2>About the site</h2>
                 </div>
                 <p>Powered by the following technologies</p>
-                <ListGroup variant="flush" horizontal>
+                <ListGroup horizontal>
                   <ListGroup.Item>React.js</ListGroup.Item>
                   <ListGroup.Item>Next.js</ListGroup.Item>
                   <ListGroup.Item>Bootstrap 4</ListGroup.Item>
